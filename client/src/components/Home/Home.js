fcc2ed63f9e1f2d38e2368512867fe964fec55be
Paritosh Mishra <86555220+paritosh921@users.noreply.github.com>
2022-02-10 11:32:49 +0530

    Initial commit

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { Container, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default Home;
